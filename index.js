346c9c1f272d937eb18ce072cbcd852e21a58fc4
Davidnarud22 <54070083+Davidnarud22@users.noreply.github.com>
2025-08-08 22:40:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/share', (req, res) => {
-  const { type, id } = req.query;
+  const { deepLink } = req.query;
 
-  if (!type || !id) {
-    return res.status(400).send('Missing type or id');
+  if (!deepLink) {
+    return res.status(400).send('Missing deepLink parameter');
   }
 
-  const deepLink = `intent://import?id=${id}&type=${type}#Intent;scheme=doobi;package=com.doobilist;end`;
-  const fallbackLink = 'https://example.com/no-play-store'; // optional
-
-  res.send(`
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <title>Opening DoobiList...</title>
-        <script>
-          setTimeout(function() {
-            window.location = "${fallbackLink}";
-          }, 1500);
-        </script>
-      </head>
-      <body style="background-color:black; color:white; text-align:center; padding-top:50px;">
-        <h2>Opening your list...</h2>
-        <a href="${deepLink}">Click here if it doesn't open automatically</a>
-        <script>
-          window.location = "${deepLink}";
-        </script>
-      </body>
-    </html>
-  `);
+  res.redirect(deepLink);
 });
 
 const PORT = process.env.PORT || 3000;
