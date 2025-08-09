2c30238b808594270efe80fc0e7af3cca013fc2a
Davidnarud22 <54070083+Davidnarud22@users.noreply.github.com>
2025-08-08 22:34:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const app = express();
-
 app.get('/share', (req, res) => {
   const { type, id } = req.query;
 
@@ -8,10 +5,8 @@ app.get('/share', (req, res) => {
     return res.status(400).send('Missing type or id');
   }
 
-  // Build the intent URI from the clean params
   const deepLink = `intent://import?id=${id}&type=${type}#Intent;scheme=doobi;package=com.doobilist;end`;
 
-  // Send HTML that only tries to open the app
   res.send(`
     <html>
       <head>
@@ -22,15 +17,9 @@ app.get('/share', (req, res) => {
         <h2>Opening your list...</h2>
         <a href="${deepLink}">Click here if it doesn't open automatically</a>
         <script>
-          // Try to open the app
           window.location = "${deepLink}";
         </script>
       </body>
     </html>
   `);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Redirect server running on port ${PORT}`);
 });
