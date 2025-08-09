d7bb93e41311443738a7eaa7a582d87736bf1910
Davidnarud22 <54070083+Davidnarud22@users.noreply.github.com>
2025-08-08 23:03:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const app = express();
 
 app.get("/share", (req, res) => {
   const { id, type } = req.query;
-console.log(id,type)
+
   if (!id || !type) {
     return res.status(400).send("Missing id or type parameter");
   }
 
-  // Redirect to your app's deep link
-  const deepLink = `doobi://import?id=${id}&type=${type}`;
-  res.redirect(deepLink);
+  // Android intent deep link
+  const intentLink = `intent://import?id=${id}&type=${type}#Intent;scheme=doobi;package=com.doobilist;end`;
+
+  res.redirect(intentLink);
 });
+
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
