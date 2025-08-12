cfc70222dd85330732ae5a705ca0d907e1841d61
Davidnarud22 <54070083+Davidnarud22@users.noreply.github.com>
2025-08-11 21:12:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-const express = require('express');
-
-const app = express();
-
-app.get("/share", (req, res) => {
-  const { id, type } = req.query;
-
-  console.log("Incoming request to /share");
-  console.log("Query params:", { id, type });
-
-  if (!id || !type) {
-    console.log("❌ Missing parameters");
-    return res.status(400).send("Missing id or type parameter");
-  }
-
-  // Android intent deep link
-  const intentLink = `intent://import?id=${id}&type=${type}#Intent;scheme=doobi;package=com.doobilist;end`;
-
-  console.log("Redirecting to:", intentLink);
-
-  res.redirect(intentLink);
-});
-
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0;url=intent://import?id=9e0d19cf-cf54-4e8c-b49b-ba4b98eaeb74&type=shopping#Intent;scheme=doobi;package=com.doobilist;end">
+  </head>
+  <body>
+    <p>Opening the app...</p>
+  </body>
+</html>
