dd08773434bea6eeb756dd0722f4c7a5a56f6b4a
Davidnarud22 <54070083+Davidnarud22@users.noreply.github.com>
2025-08-11 21:17:38 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="refresh" content="0;url=intent://import?id=9e0d19cf-cf54-4e8c-b49b-ba4b98eaeb74&type=shopping#Intent;scheme=doobi;package=com.doobilist;end">
-  </head>
-  <body>
-    <p>Opening the app...</p>
-  </body>
-</html>
